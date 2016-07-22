99f21365283b1dc860255181c1a0a1c2a8014f25
SarahHartwick <sarah@surfsetfitness.com>
2016-07-22 09:24:57 -0400

    Ruby RPN Challenge
    Works for one operator

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,3 +1,7 @@
 def calc(expr)
-  # Your code here
+  if expr.length == 0
+    return 0
+  end
+  operator = expr.split(' ')[-1].to_sym
+  return expr.split(' ')[0].to_f.public_send(operator, expr.split(' ')[1].to_f)
 end
